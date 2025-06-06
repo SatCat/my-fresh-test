c6f69c4a46a38ed494b42ef331fcfe007a52b651
SatCat <39083134+SatCat@users.noreply.github.com>
2025-06-06 09:20:08 +0000

    cron

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,6 +1,10 @@
 import { useSignal } from "@preact/signals";
 import Counter from "../islands/Counter.tsx";
 
+Deno.cron("Log a message", "*/3 * * * *", () => {
+  console.log("Ping");
+});
+
 export default function Home() {
   const count = useSignal(3);
   return (
